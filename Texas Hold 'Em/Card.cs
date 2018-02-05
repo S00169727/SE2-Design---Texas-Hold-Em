4836104a8584e6dca04d5c7e98144d9634f622bb
Conor Fearon <S00169727@mail.itsligo.ie>
2018-02-05 15:12:08 +0000

    Created a Card and Deck class with methods for a basic card game.

---
+++ Texas Hold 'Em/Card.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Texas_Hold__Em
+{
+    public enum CardSuits { spades, hearts, diamonds, clubs }
+    public enum NumberValue { Ace = 1, Two = 2, Three = 3, Four = 4, Five = 5, Six = 6, Seven = 7, Eight = 8, Nine = 9, Ten = 10, Jack = 11, Queen = 12, King = 13 }
+
+    class Card
+    {
+        public NumberValue Rank { get; set; }
+        public CardSuits Suit { get; set; }
+
+        public Card(NumberValue r, CardSuits s)
+        {
+            Rank = r;
+            Suit = s;
+        }
+
+
+        public override string ToString()
+        {
+            return string.Format($"{Enum.GetName(typeof(CardSuits),Suit)}, {Enum.GetName(typeof(NumberValue),Rank)}");
+        }
+    }
+}
---
+++ Texas Hold 'Em/Deck.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Texas_Hold__Em
+{
+    class Deck: List<Card>
+    {
+        public Deck()
+        {
+            this.Reset();
+        }
+
+        public bool IsEmpty()
+        {
+            if (this.Count == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void Shuffle()
+        {
+            List<Card> tempList = new List<Card>();
+
+            Card tempCard;
+
+            Random r = new Random();
+
+            while (this.Count > 0)
+            {
+                tempCard = this[r.Next(this.Count)];
+                tempList.Add(tempCard);
+                this.Remove(tempCard);
+            }
+        }
+
+        public void Reset()
+        {
+            Card c;
+
+            this.Clear();
+
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 14; j++)
+                {
+                    c = new Card((NumberValue)j, (CardSuits)i);
+                    this.Add(c);
+                }
+            }
+        }
+    }
+}
